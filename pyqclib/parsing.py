1dd6df3996f8036546e00af063fcbe847b4d2d80
Björn Dahlgren <bjodah@gmail.com>
2013-01-21 20:28:00 +0100

    Cleanup of pyqclib/parsing.py

--- pyqclib/parsing.py
+++ pyqclib/parsing.py
@@ -40,7 +40,6 @@ class Property(object):
         return self._key
 
 
-
 class QCResult(object):
     """
     Class to represent a result from a QC computation
@@ -64,10 +63,13 @@ class QCResult(object):
 
     def scfenergy(self, index = -1):
         if isinstance(index, basestring): index = int(index) # Neglects string slices
-        return (self.cclib_data.scfenergies[index] * pq.eV * pq.constants.Avogadro_constant).rescale(self.default_units['energy'])
+        return (self.cclib_data.scfenergies[index] * pq.eV * \
+                pq.constants.Avogadro_constant).rescale(
+            self.default_units['energy'])
+
     scfenergy.returns_with_unit = True
-    #scfenergy.unit = default_energy_unit
     scfenergy.unit_type = 'energy' # key in defs.UNITS
+
 
     def vibtemps(self):
         try:
@@ -147,158 +149,20 @@ class QCResult(object):
             self._cclib_data = _get_parsed_cclib_data(self._logfile)
         return self._cclib_data
 
-    # @property
-    # def E(self):
-    #     """Transparantly resolve result_type to most probable energy metric"""
-    #     res = self.result_type_resolution[self._result_type]['E']
-    #     if res != None:
-    #         return getattr(self, res)
 
+# class NonExistentQCResult(QCResult):
 
-    # def parse_extrapol_prop(self, prop):
-    #     for line in file(self._logfile, 'rt'):
-    #         if line[0] != '\#':
-    #             tok, val = line.split(':')
-    #             if tok == prop:
-    #                 return float(val)
+#     def __init__(self, collect_dir = None):
+#         self._collect_dir = collect_dir
+#         return None
 
+#     def __getitem__(self, key):
+#         return NonExistentStructure(self._collect_dir)
 
-    # @property
-    # def extrapol_E(self):
-    #     return self.parse_extrapol_prop('E') * kilojoule / pq.mol
+#     def __getattr__(self, attr):
+#         return None
 
-    # @property
-    # def extrapol_G(self):
-    #     return self.parse_extrapol_prop('G') * kilojoule / pq.mol
-
-    # @property
-    # def extrapol_S2(self):
-    #     return self.parse_extrapol_prop('S2')
-
-
-
-
-    # @property
-    # #@memoize
-    # def ccsd_t_energy(self):
-    #     try:
-    #         return parse_gaussian.get_gaussian_ccsd_t(self._logfile)
-    #     except IOError:
-    #         print('No CCSD(T) energy found in: ' + self._logfile)
-    #         return None
-
-    # @property
-    # #@memoize
-    # def ump2_energy(self):
-    #     try:
-    #         return parse_gaussian.get_gaussian_eump2(self._logfile)
-    #     except IOError:
-    #         print('No EUMP2 energy found in: ' + self._logfile)
-    #         return None
-
-    # @property
-    # #@memoize
-    # def scs_ump2_energy(self):
-    #     try:
-    #         return parse_gaussian.get_gaussian_scs_eump2(self._logfile)
-    #     except IOError:
-    #         print('No spin component data / EUMP2 energy found in: ' + self._logfile)
-    #         return None
-
-    # @property
-    # #@memoize
-    # def T1(self):
-    #     return parse_gaussian.get_gaussian_after_lineleading_token(
-    #         self._logfile, ' T1 Diagnostic =')
-
-
-    # @property
-    # #@memoize
-    # def S2(self):
-    #     res = self.result_type_resolution[self._result_type]['S2']
-    #     if res != None:
-    #         return getattr(self, res)
-
-    # @property
-    # #@memoize
-    # def gaussian_S2(self):
-    #     try:
-    #         return parse_gaussian.get_gaussian_res(self._logfile,
-    #                                                token = '\\S2=')
-    #     except IOError:
-    #         print('No \S2 found in: ' + self._logfile)
-    #         return None
-
-
-    # @property
-    # #@memoize
-    # def HF(self):
-    #     return parse_gaussian.get_gaussian_scf(self._logfile)
-
-
-    # def get_E_vib(self, T):
-    #     vt = self.real_vibtemps
-    #     if vt == None: return None
-    #     NA = pq.constants.Avogadro_constant
-    #     kB = pq.constants.Boltzmann_constant
-
-    #     if T < 1e-3 * pq.K:
-    #         Evib = NA * kB * np.sum(vt / 2)
-    #     else:
-    #         Evib = NA * kB * np.sum(vt / 2 + vt * np.exp( - vt / T) /(1 - np.exp( - vt / T)))
-
-    #     return Evib.rescale(kilojoule / pq.mol)
-
-
-    # @property
-    # #@memoize
-    # def ZPVE(self):
-    #     self.get_E_vib(0 * pq.Kelvin)
-
-    # @property
-    # #@memoize
-    # def E_with_ZPVE(self):
-    #     return self.E + self.ZPVE
-
-    # def get_G_elec_vib(self, T):
-    #     print self._name, self._logfile
-    #     print 'E', self.E
-    #     print 'E_vib', self.get_E_vib(T)
-    #     print 'T', T
-    #     print 'S_vib', self.get_S_vib(T)
-    #     E_vib = self.get_E_vib(T)
-    #     S_vib = self.get_S_vib(T)
-    #     if None in [E_vib, S_vib]: return None
-    #     return self.E + E_vib - T * S_vib
-
-    # @property
-    # #@memoize
-    # def G_elec_vib_std(self):
-    #     return self.get_G_elec_vib(self._T_ref)
-
-    # @property
-    # #@memoize
-    # def G(self):
-    #     """ Transparantly resolve result_type to most probable free energy metric"""
-    #     print self._name, self._logfile, self._result_type
-    #     res = self.result_type_resolution[self._result_type]['G']
-    #     if res != None:
-    #         return getattr(self, res)
-
-
-class NonExistentQCResult(QCResult):
-
-    def __init__(self, collect_dir = None):
-        self._collect_dir = collect_dir
-        return None
-
-    def __getitem__(self, key):
-        return NonExistentStructure(self._collect_dir)
-
-    def __getattr__(self, attr):
-        return None
-
-    @property
-    def _logfile(self):
-        return self._collect_dir
+#     @property
+#     def _logfile(self):
+#         return self._collect_dir
 
